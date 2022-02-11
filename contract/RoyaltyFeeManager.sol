a5badc2e62bb57f5880533e3599784a2d7abca37
tsai50702 <37826487+tsai50702@users.noreply.github.com>
2022-02-11 12:28:51 +0800

    royalty fee contract update

---
+++ contract/RoyaltyFeeManager.sol
@@ -0,0 +1,78 @@
+pragma solidity ^0.8.0;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC165, IERC2981} from "@openzeppelin/contracts/interfaces/IERC2981.sol";
+
+pragma solidity ^0.8.0;
+
+interface IRoyaltyFeeManager {
+    function calculateRoyaltyFeeAndGetRecipient(
+        address collection,
+        uint256 tokenId,
+        uint256 amount
+    ) external view returns (address, uint256);
+}
+pragma solidity ^0.8.0;
+
+interface IRoyaltyFeeRegistry {
+    function updateRoyaltyInfoForCollection(
+        address collection,
+        address setter,
+        address receiver,
+        uint256 fee
+    ) external;
+
+    function updateRoyaltyFeeLimit(uint256 _royaltyFeeLimit) external;
+
+    function royaltyInfo(address collection, uint256 amount) external view returns (address, uint256);
+
+    function royaltyFeeInfoCollection(address collection)
+        external
+        view
+        returns (
+            address,
+            address,
+            uint256
+        );
+}
+/**
+ * @title RoyaltyFeeManager
+ * @notice It handles the logic to check and transfer royalty fees (if any).
+ */
+contract RoyaltyFeeManager is IRoyaltyFeeManager, Ownable {
+    // https://eips.ethereum.org/EIPS/eip-2981
+    bytes4 public constant INTERFACE_ID_ERC2981 = 0x2a55205a;
+
+    IRoyaltyFeeRegistry public immutable royaltyFeeRegistry;
+
+    /**
+     * @notice Constructor
+     * @param _royaltyFeeRegistry address of the RoyaltyFeeRegistry
+     */
+    constructor(address _royaltyFeeRegistry) {
+        royaltyFeeRegistry = IRoyaltyFeeRegistry(_royaltyFeeRegistry);
+    }
+
+    /**
+     * @notice Calculate royalty fee and get recipient
+     * @param collection address of the NFT contract
+     * @param tokenId tokenId
+     * @param amount amount to transfer
+     */
+    function calculateRoyaltyFeeAndGetRecipient(
+        address collection,
+        uint256 tokenId,
+        uint256 amount
+    ) external view override returns (address, uint256) {
+        // 1. Check if there is a royalty info in the system
+        (address receiver, uint256 royaltyAmount) = royaltyFeeRegistry.royaltyInfo(collection, amount);
+
+        // 2. If the receiver is address(0), fee is null, check if it supports the ERC2981 interface
+        if ((receiver == address(0)) || (royaltyAmount == 0)) {
+            if (IERC165(collection).supportsInterface(INTERFACE_ID_ERC2981)) {
+                (receiver, royaltyAmount) = IERC2981(collection).royaltyInfo(tokenId, amount);
+            }
+        }
+        return (receiver, royaltyAmount);
+    }
+}
